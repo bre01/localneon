94a87f1ea13f02c44f68ec2ce05b1750ae83b62a
bre <iwanttosignup321@gmail.com>
2024-05-31 10:23:09 +0800

    sync:

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,43 @@
 
 
 
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { products } from './db/schema';
+import { Hono } from 'hono';
+import { PgSelect } from 'drizzle-orm/pg-core';
 
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('very hot indeed');
-	},
-};
+export type Env = {
+	DATABASE_URL: string;
+  };
+  
+  const app = new Hono<{ Bindings: Env }>();
+  
+  app.get('/', async (c) => {
+	const sql = neon(c.env.DATABASE_URL);
+	const db = drizzle(sql);
+	const result = await db.select().from(products);
+	return c.json({ result });
+  });
+  
+  app.get('/products',async(c)=>{
+	const sql=neon(c.env.DATABASE_URL);
+	const db=drizzle(sql);
+	const results:Promise<{price:number|null,name:string|null}[]>=db.select({price:products.price,name:products.name}).from(products);
+	return c.json({ results:await results})
+  }
+)
+  
+
+  
+  app.onError((error, c) => {
+	console.log(error)
+	return c.json({ error }, 400)
+  })
+
+export default app;
+
+
+
+
